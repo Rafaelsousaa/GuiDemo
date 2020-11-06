0d91588aa2749b4d5415be54601db961a4a965ec
Rafaelsousaa <rafael.current@gmail.com>
2020-11-06 10:50:48 +0000

    version2
    testing

--- src/guidemo/GuiDemo.java
+++ src/guidemo/GuiDemo.java
@@ -16,6 +16,10 @@ public class GuiDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
     }
-    
+
 }
